7836d22ddee1ddd91857c4897f7df97136310923
DanielPrzewozny <daniel.przewozny@yahoo.com>
2023-03-28 17:28:48 +0200

    Max zero gap in binary format (binary test added)

---
+++ xUnit_Samples/xUnitTests/Binary_xUnit_Test.cs
@@ -0,0 +1,19 @@
+﻿using Tasks._4___BinaryGap;
+
+namespace xUnitTests;
+
+public class Binary_xUnit_Test
+{
+    [Theory]
+    [InlineData(8833, 6, true)]
+    [InlineData(4368, 4, true)]
+    [InlineData(260, 6, false)]
+    public void CalculateMaxBinaryGap(int input, int maxGap, bool expectedResult)
+    {
+        //Act
+        var maxBinaryGap = BinaryExtensions.GetMaxBinaryGap(input);
+
+        //Assert
+        Assert.Equal(maxBinaryGap == maxGap, expectedResult);
+    }
+}
